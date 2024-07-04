5110090ba2a8614ab26ac17f4bba948a6b1f6005
SnezanaBogdanovic <snezanabogdanovic79@gmail.com>
2024-07-04 16:30:28 +0200

    5.5 respown

--- TDgame/Arena.java
+++ TDgame/Arena.java
@@ -13,6 +13,13 @@ public class Arena extends World
      * Constructor for objects of class MyWorld.
      * 
      */
+    public void respawn(Enemy e)
+    {
+        e=new Enemy(5,3);
+        e.setLocation(0, 5);
+        e.setRotation(0);
+        this.addObject(e,0,5);
+    }
     public Arena()
     {    
         // Create a new world with 24x12 cells with a cell size of 50x50 pixels.
