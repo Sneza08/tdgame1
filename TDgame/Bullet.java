43375fb0b5a51d45dd8e58f45394ba6f94386f88
SnezanaBogdanovic <snezanabogdanovic79@gmail.com>
2024-07-04 17:00:02 +0200

    5.11. speed

--- TDgame/Bullet.java
+++ TDgame/Bullet.java
@@ -12,6 +12,13 @@ public class Bullet extends Actor
      * Act - do whatever the Bullet wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private int speed;
+    
+    public Bullet(int speed)
+    {
+        this.speed=speed;
+        this.setRotation(180);
+    }
     public void act()
     {
         // Add your action code here.
